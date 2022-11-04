571682406ee4a85af98a3acd1ccc5717f28b150e
Guilherme Gazzo <guilherme@gazzo.xyz>
2022-11-04 00:03:22 -0300

    test

---
+++ src/handleQALabels.ts
@@ -0,0 +1,55 @@
+import { Context } from "probot";
+
+export const applyLabels = async (
+  context: Context<
+    | "pull_request.opened"
+    | "pull_request.synchronize"
+    | "pull_request.labeled"
+    | "pull_request.unlabeled"
+  >
+) => {
+  try {
+    const issue = context.payload.pull_request;
+
+    const originalLabels = issue.labels.map((label) => label.name);
+    const tested = Boolean(originalLabels.includes("stat: QA tested"));
+    const skipped = Boolean(originalLabels.includes("stat: QA skipped"));
+
+    const labels: string[] = [
+      ...new Set([...originalLabels, "stat: needs QA", "stat: ready to merge"]),
+    ].filter((label) => {
+      if (label === "stat: needs QA") {
+        return !(skipped || tested);
+      }
+
+      if (label === "stat: QA skipped") {
+        return !tested && skipped;
+      }
+
+      if (label === "stat: ready to merge") {
+        return skipped || tested;
+      }
+      return true;
+    });
+
+    if (
+      labels.length === originalLabels.length &&
+      labels.every((label) => originalLabels.includes(label))
+    ) {
+      return;
+    }
+    const pull = context.pullRequest({
+      labels,
+    });
+    console.log(pull);
+
+    await context.octokit.issues.addLabels(
+      context.issue({
+        labels,
+      })
+    );
+  } catch (error) {
+    console.log(error);
+    // error instanceof Error && core.setFailed(error.message);
+  }
+};
--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,5 @@
-import { Context, Probot } from "probot";
-
-const applyLabels = async (
-  context: Context<
-    | "pull_request.opened"
-    | "pull_request.synchronize"
-    | "pull_request.labeled"
-    | "pull_request.unlabeled"
-  >
-) => {
-  try {
-    const issue = context.payload.pull_request;
-
-    const originalLabels = issue.labels.map((label) => label.name);
-    const tested = Boolean(originalLabels.includes("stat: QA tested"));
-    const skipped = Boolean(originalLabels.includes("stat: QA skipped"));
-
-    const labels: string[] = [
-      ...new Set([...originalLabels, "stat: needs QA", "stat: ready to merge"]),
-    ].filter((label) => {
-      if (label === "stat: needs QA") {
-        return !(skipped || tested);
-      }
-
-      if (label === "stat: QA skipped") {
-        return !tested && skipped;
-      }
-
-      if (label === "stat: ready to merge") {
-        return skipped || tested;
-      }
-      return true;
-    });
-
-    if (
-      labels.length === originalLabels.length &&
-      labels.every((label) => originalLabels.includes(label))
-    ) {
-      return;
-    }
-    const pull = context.pullRequest({
-      labels,
-    });
-    console.log(pull);
-
-    await context.octokit.issues.addLabels(
-      context.issue({
-        labels,
-      })
-    );
-  } catch (error) {
-    console.log(error);
-    // error instanceof Error && core.setFailed(error.message);
-  }
-};
+import { Probot } from "probot";
+import { applyLabels } from "./handleQALabels";
 
 export = (app: Probot) => {
   app.on(
@@ -76,7 +23,7 @@ export = (app: Probot) => {
 
     return context.octokit.checks.create(
       context.repo({
-        name: "My app!",
+        name: "Rocket.Chat PR Check - by Dionisio",
         head_branch: headBranch,
         head_sha: headSha,
         status: "completed",
@@ -84,8 +31,11 @@ export = (app: Probot) => {
         conclusion: "success",
         completed_at: new Date(),
         output: {
-          title: "Probot check!",
-          summary: "The check has passed!",
+          title: "All the Tests are ok!",
+          summary: `
+          - :checkered_flag: Pull Request title follows the conventions
+          - :checkered_flag: Labels are properly set
+          `,
         },
       })
     );
