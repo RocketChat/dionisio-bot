7c8635838bc54060afd03a520f6269c4eaf0b031
Guilherme Gazzo <guilherme@gazzo.xyz>
2022-11-07 16:27:39 -0300

    handle conflicts

--- src/handleQALabels.ts
+++ src/handleQALabels.ts
@@ -11,13 +11,25 @@ export const applyLabels = async (
   try {
     const issue = context.payload.pull_request;
 
+    const hasConflicts =
+      context.payload.pull_request.mergeable_state === "dirty";
+
     const originalLabels = issue.labels.map((label) => label.name);
     const tested = Boolean(originalLabels.includes("stat: QA tested"));
     const skipped = Boolean(originalLabels.includes("stat: QA skipped"));
 
     const labels: string[] = [
-      ...new Set([...originalLabels, "stat: needs QA", "stat: ready to merge"]),
+      ...new Set([
+        ...originalLabels,
+        "stat: needs QA",
+        "stat: ready to merge",
+        "stat: conflict",
+      ]),
     ].filter((label) => {
+      if (label === "stat: conflict") {
+        return hasConflicts;
+      }
+
       if (label === "stat: needs QA") {
         return !(skipped || tested);
       }
@@ -27,7 +39,7 @@ export const applyLabels = async (
       }
 
       if (label === "stat: ready to merge") {
-        return skipped || tested;
+        return !hasConflicts && (skipped || tested);
       }
       return true;
     });
