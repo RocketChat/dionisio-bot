177e69e8c94838ba1025ca64268ad7db8464d378
Guilherme Gazzo <guilhermegazzo@gmail.com>
2024-06-12 14:11:22 -0300

    ..

--- src/handleBackport.ts
+++ src/handleBackport.ts
@@ -31,78 +31,82 @@ export const handleBackport = async ({
 
   // Filter out the tags that are already in the project
 
-  await Promise.allSettled(
-    tags.map(async (tag): Promise<void> => {
-      const result = await context.octokit.repos
-        .getReleaseByTag({
-          ...context.repo(),
-          tag,
-        })
-        .catch(() => undefined);
+  try {
+    await Promise.allSettled(
+      tags.map(async (tag): Promise<void> => {
+        const result = await context.octokit.repos
+          .getReleaseByTag({
+            ...context.repo(),
+            tag,
+          })
+          .catch(() => undefined);
 
-      if (result?.data) {
-        await context.octokit.issues.createComment({
-          ...context.issue(),
-          body: `${tag} already exists in the project`,
-        });
-        return;
-      }
-
-      const ver = semver.patch(tag) - 1;
-
-      if (ver <= 0) {
-        return;
-      }
-
-      const previousTag =
-        semver.major(tag) + "." + semver.minor(tag) + "." + ver;
-
-      try {
-        await context.octokit.repos.getReleaseByTag({
-          ...context.repo(),
-          tag: previousTag,
-        });
-      } catch (err) {
-        console.log("Failed to get previous tag", previousTag, err);
-        throw err;
-      }
-      try {
-        await upsertProject(
-          context,
-          tag,
-          consoleProps("upsertProject", {
-            id: pr.node_id,
-            sha: pr.merge_commit_sha,
-            title: pr.title,
-            number: pr.number,
-            author: pr.author,
-          }),
-          previousTag,
-          assignee
-        );
-      } catch (err) {
-        if (err instanceof ErrorCherryPickConflict) {
-          context.octokit.issues.createComment({
+        if (result?.data) {
+          await context.octokit.issues.createComment({
             ...context.issue(),
-            body: `
-Sorry, I couldn't do that backport because of conflicts. Could you please solve them?
-
-you can do so by running the following commands:
-\`\`\`
-git fetch
-git checkout ${err.arg.head}
-git cherry-pick ${err.arg.commits.join(" ")}
-// solve the conflict
-git push
-\`\`\`
-
-
-after that just run \`/backport ${tag}\` again
-`,
+            body: `${tag} already exists in the project`,
           });
+          return;
         }
-        console.log(err);
-      }
-    })
-  );
+
+        const ver = semver.patch(tag) - 1;
+
+        if (ver <= 0) {
+          return;
+        }
+
+        const previousTag =
+          semver.major(tag) + "." + semver.minor(tag) + "." + ver;
+
+        try {
+          await context.octokit.repos.getReleaseByTag({
+            ...context.repo(),
+            tag: previousTag,
+          });
+        } catch (err) {
+          console.log("Failed to get previous tag", previousTag, err);
+          throw err;
+        }
+        try {
+          await upsertProject(
+            context,
+            tag,
+            consoleProps("upsertProject", {
+              id: pr.node_id,
+              sha: pr.merge_commit_sha,
+              title: pr.title,
+              number: pr.number,
+              author: pr.author,
+            }),
+            previousTag,
+            assignee
+          );
+        } catch (err) {
+          if (err instanceof ErrorCherryPickConflict) {
+            context.octokit.issues.createComment({
+              ...context.issue(),
+              body: `
+  Sorry, I couldn't do that backport because of conflicts. Could you please solve them?
+  
+  you can do so by running the following commands:
+  \`\`\`
+  git fetch
+  git checkout ${err.arg.head}
+  git cherry-pick ${err.arg.commits.join(" ")}
+  // solve the conflict
+  git push
+  \`\`\`
+  
+  
+  after that just run \`/backport ${tag}\` again
+  `,
+            });
+          }
+          console.log(err);
+        }
+      })
+    );
+  } catch (err) {
+    console.log(err);
+  }
 };
